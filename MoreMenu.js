414137fd579dd53d50685d07776f728d7f5f6728
MusikAnimal <ziemba0123@gmail.com>
2015-02-17 12:59:33 -0500

    remove commented out code

--- MoreMenu.js
+++ MoreMenu.js
@@ -240,11 +240,6 @@
 
   var pageMenuList = {
     'Page' : {
-      // 'More' : {
-      //   'Placeholder' : {
-      //     url : '#'
-      //   }
-      // },
       'Page logs' : {
         'All logs' : {
           url : mw.util.getUrl( 'Special:Log', { action: 'view', page: pageName } )
@@ -423,7 +418,6 @@
     $( '#p-views ul' ).on( 'beforeTabCollapse', function() {
       if ( $( '#ca-history' ).hasClass( 'collapsible' ) ) {
         $( '#p-page2' ).find( 'ul' ).append( $( '#ca-history' ).detach() );
-        // removeCactions();
       }
     } );
   }
@@ -470,25 +464,6 @@
         }
       } );
     }
-
-    // Absorb the More (cactions) menu unless user disables it
-    // if ( $.jStorage.get( 'mmNoAbsorbtion' ) ) {
-    //   $( '#c2-page-more' ).remove();
-    // } else {
-    //   var $cactions = $( '#p-cactions' ).find( 'ul' ),
-    //     $newCactions = $( '#c2-page-more' ).prop( 'id', 'p-cactions' );
-
-    //   $( '#p-cactions' ).remove();
-    //   $newCactions.find( 'ul' ).replaceWith($cactions);
-    //   $newCactions.find( 'ul' ).append( '<li id="c2-unabsorb"><a><small>Disable this feature</small></a></li>' );
-
-    //   $( '#c2-unabsorb' ).on( 'click', function() {
-    //     if ( confirm( 'Hit OK to refresh the page with the More menu back at it\'s own tab.\n\nThis setting will persist only in this browser session. To re-enable, clear your browser\'s cache.' ) ) {
-    //       $.jStorage.set( 'mmNoAbsorbtion', true );
-    //       document.location.reload();
-    //     }
-    //   } );
-    // }
   }
 
 
@@ -620,14 +595,6 @@
     }
   }
 
-  function removeCactions() {
-    return true;
-    // Comment out for now, conflicts with other scripts
-    // if( $( '#p-cactions' ).find( 'li' ).length === 0 ) {
-    //   $( '#p-cactions' ).remove();
-    // }
-  }
-
   function sanitize( name ) {
     return name.toLowerCase().replace( / /g, '_' );
   }
@@ -709,8 +676,6 @@
       }
 
       if ( typeof fn === 'function' ) fn( data, userPermissions );
-
-      // removeCactions();
     } );
   }
 } )( );
