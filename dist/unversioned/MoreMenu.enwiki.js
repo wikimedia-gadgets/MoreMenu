5a17ab7357426a7efaf0e2b351bda82f7de750a4
MusikAnimal <musikanimal@gmail.com>
2019-12-18 00:13:27 -0500

    Remove MoreMenu.enwiki.js from build step

--- dist/unversioned/MoreMenu.enwiki.js
+++ dist/unversioned/MoreMenu.enwiki.js
@@ -1,27 +1,11 @@
 /**
-* WARNING: GLOBAL GADGET FILE
-* Compiled from source at https://github.com/MusikAnimal/MoreMenu
-* Please submit code changes as a pull request to the source repository at https://github.com/MusikAnimal/MoreMenu
-* Are there missing translations? See [[meta:MoreMenu#Localization]].
-* Want to add custom links? See [[meta:MoreMenu#Customization]].
-* 
-* Script:         MoreMenu.js
-* Version:        5.0.9
-* Author:         MusikAnimal
-* License:        MIT
-* Documentation:  [[meta:MoreMenu]]
-* GitHub:         https://github.com/MusikAnimal/MoreMenu
-* Skins:          Vector, Timeless, Monobook, Modern
-* Browsers:       See [[mw:Compatibility#Browsers]]
-**/
-"use strict";
-
-/**
+ * WARNING: GLOBAL GADGET FILE
+ * Compiled from source at https://github.com/MusikAnimal/MoreMenu
+ * Please submit code changes as a pull request to the source repository at https://github.com/MusikAnimal/MoreMenu
+ *
  * Enwiki extension to MoreMenu.
  * This adds a menu item with RfAs/RfBs and an item for XfD where applicable.
  */
-
-/* eslint-disable max-len */
 $(function () {
   /**
    * Look for and add links to RfAs, RfBs, Arbitration cases, etc.
@@ -56,15 +40,15 @@ $(function () {
     var links = {};
     api.get({
       titles: Object.keys(rfxs).map(function (rfx) {
-        return "".concat(rfx, "/").concat(config.targetUser.name);
+        return rfx + '/' + config.targetUser.name;
       }).join('|'),
       formatversion: 2
     }).done(function (data) {
       data.query.pages.forEach(function (page) {
         if (!page.missing) {
-          var key = rfxs[page.title.replace("/".concat(config.targetUser.name), '')];
+          var key = rfxs[page.title.replace('/' + config.targetUser.name, '')];
           links[key] = {
-            url: mw.util.getUrl("Special:PrefixIndex/".concat(page.title))
+            url: mw.util.getUrl('Special:PrefixIndex/' + page.title)
           };
         }
       });
@@ -74,16 +58,18 @@ $(function () {
       }
     });
   }
+
   /**
    * Look for and add a link to Special:PrefixIndex for AfDs or XfDs.
    * @param {mw.Api} api
    * @param {Object} config
    */
-
-
   function addXfD(api, config) {
     api.get({
-      titles: ["Wikipedia:Articles for deletion/".concat(config.page.name), "Wikipedia:Miscellany for deletion/".concat(config.page.name)].join('|'),
+      titles: [
+        'Wikipedia:Articles for deletion/' + config.page.name,
+        'Wikipedia:Miscellany for deletion/' + config.page.name
+      ].join('|'),
       prop: 'info',
       formatversion: 2
     }).done(function (data) {
@@ -92,15 +78,13 @@ $(function () {
           return false;
         }
 
-        var link = mw.util.getUrl("Special:PrefixIndex/".concat(page.title));
+        var link = mw.util.getUrl('Special:PrefixIndex/' + page.title);
 
         switch (page.title.split('/')[0]) {
           case 'Wikipedia:Miscellany for deletion':
             return MoreMenu.addLink('page', 'MfDs', link);
-
           case 'Wikipedia:Articles for deletion':
             return MoreMenu.addLink('page', 'AfDs', link);
-
           default:
             return false;
         }
@@ -118,20 +102,18 @@ $(function () {
     if (config.page.name) {
       addXfD(api, config);
     }
-    /** Add link to BLP edits in the 'Analysis' menu. */
 
-
+    // Add link to BLP edits in the 'Analysis' menu.
     if (!config.targetUser.ipRange) {
-      MoreMenu.addSubmenuLink('user', 'analysis', 'BLP Edits', "https://xtools.wmflabs.org/categoryedits/".concat(config.project.serverName, "/").concat(config.targetUser.encodedName, "/Living people"));
-      /** Add link to AfD stats. */
+      MoreMenu.addSubmenuLink('user', 'analysis', 'BLP Edits', 'https://xtools.wmflabs.org/categoryedits/' + config.project.serverName + '/' + config.targetUser.encodedName + '/Living people'));
 
-      MoreMenu.addSubmenuLink('user', 'analysis', 'AfD stats', "https://tools.wmflabs.org/afdstats/afdstats.py?name=".concat(config.targetUser.encodedName), 'analysis-xtools');
+      // Add link to AfD stats.
+      MoreMenu.addSubmenuLink('user', 'analysis', 'AfD stats', 'https://tools.wmflabs.org/afdstats/afdstats.py?name=' + config.targetUser.encodedName, 'analysis-xtools');
     }
-    /** Add link to Peer reviewer tool under 'Tools'. */
 
-
+    // Add link to Peer reviewer tool under 'Tools'.
     MoreMenu.addSubmenuItem('page', 'tools', 'Peer reviewer', {
-      url: "https://dispenser.info.tm/~dispenser/view/Peer_reviewer#page:".concat(config.page.encodedName),
+      url: 'https://dispenser.info.tm/~dispenser/view/Peer_reviewer#page:' + config.page.encodedName,
       pageExists: true,
       databaseRestrict: ['enwiki'],
       namespaceRestrict: [0, 2, 118]
