97a7a8f5789e973b729e5e485dcfde71d94bebb1
MusikAnimal <musikanimal@gmail.com>
2019-12-18 21:17:34 -0500

    5.1.1

--- dist/MoreMenu.user.js
+++ dist/MoreMenu.user.js
@@ -6,7 +6,7 @@
 * Want to add custom links? See [[meta:MoreMenu#Customization]].
 * 
 * Script:         MoreMenu.js
-* Version:        5.1.0
+* Version:        5.1.1
 * Author:         MusikAnimal
 * License:        MIT
 * Documentation:  [[meta:MoreMenu]]
