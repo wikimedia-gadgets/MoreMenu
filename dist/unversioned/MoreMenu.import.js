5adec11dd1cccd1d2541e964cf29f88a27b65864
MusikAnimal <musikanimal@gmail.com>
2022-01-21 23:13:35 -0500

    MoreMenu.import.js: now requires mediawiki.Title

--- dist/unversioned/MoreMenu.import.js
+++ dist/unversioned/MoreMenu.import.js
@@ -4,6 +4,6 @@
  * THIS SHOULD NOT BE USED IN A PRODUCTION GADGET.
  * See [[meta:MoreMenu]] for documentation.
  */
-mw.loader.using(['mediawiki.user', 'mediawiki.util', 'mediawiki.api'], function () {
+mw.loader.using(['mediawiki.user', 'mediawiki.util', 'mediawiki.api', 'mediawiki.Title'], function () {
     mw.loader.load('https://meta.wikimedia.org/w/load.php?modules=ext.gadget.MoreMenu-beta');
 });
